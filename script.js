f1d953d2d67cb8411fee7641330749950ab02f99
Luis A.Tavarez <51054204+uppy19d0@users.noreply.github.com>
2023-10-28 00:40:57 -0400

    Update script.js
    **Código Original**:
    - Initial setup:
        - Set the dimensions of the "logo" element.
    - Project lookup function:
        - Define a function to search for project information based on user input.
        - Handle empty input with an alert.
        - Show project-specific content for "amazon" and "searchengine" projects.
    - Mobile menu and search form handling:
        - Create a responsive menu icon that toggles the search form visibility.
        - Ensure the search form is hidden by default on small screens.
    - Responsive behavior:
        - Adjust the search form's visibility based on the window size.

--- script.js
+++ script.js
@@ -3,46 +3,41 @@ let logoimg = document.getElementById("logo");
 logoimg.style.height = "250px";
 logoimg.style.width = "100%";
 
-/*this file is mainly made for the search tab for searching the project*/
-let projectName;
+
+// Search function for project lookup
 function findProject() {
-    projectName = document.getElementById("proname").value.trim(); // Trim whitespace from the input
-
-    if (projectName === "") {
-        alert("Please enter a project name."); // Show an alert for empty input
-        return; // Exit the function to prevent further execution
+    const projectNameInput = document.getElementById("proname").value.trim();
+    
+    if (projectNameInput === "") {
+        alert("Please enter a project name.");
+        return;
     }
 
-    if (projectName.toLowerCase() === "amazon") {
-        let findCard = document.getElementById("amazon");
-        document.body.innerHTML = findCard.innerHTML;
-    } else if (projectName.toLowerCase() === "searchengine") {
-        let findCard = document.getElementById("searchengine");
-        document.body.innerHTML = findCard.innerHTML;
-    } else {
-        let img = new Image();
-        img.src = 'Notfound-img.jpg';
-        document.getElementById('body').appendChild(img);
-    }
+    const projectMappings = {
+        amazon: "amazon",
+        searchengine: "searchengine",
+    };
+
+    const projectNameLower = projectNameInput.toLowerCase();
+    const findCardId = projectMappings[projectNameLower] || 'notfound-img';
+
+    const findCard = document.getElementById(findCardId);
+    document.body.innerHTML = findCard.innerHTML;
 }
 
+// Toggle search form visibility
 const menuIcon = document.getElementById("menu-icon");
 const searchForm = document.querySelector(".search-form");
 
 menuIcon.addEventListener("click", function () {
-    searchForm.style.display = (searchForm.style.display === "none" || searchForm.style.display === "") ? "flex" : "none";
+    searchForm.style.display = searchForm.style.display === "none" || searchForm.style.display === "" ? "flex" : "none";
 });
 
-// Ensure that the search form is hidden initially on small screens
-if (window.innerWidth <= 767) {
-    searchForm.style.display = "none";
+function toggleSearchFormVisibility() {
+    searchForm.style.display = window.innerWidth > 767 ? "flex" : "none";
 }
 
-window.addEventListener("resize", function () {
-    if (window.innerWidth > 767) {
-        searchForm.style.display = "flex";
-    } else {
-        searchForm.style.display = "none";
-    }
-});
+window.addEventListener("resize", toggleSearchFormVisibility);
+toggleSearchFormVisibility();
+
 
